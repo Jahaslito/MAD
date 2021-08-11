b0d563613c6a4b2798c8ce8445234e5563114956
amengemeda <amentamene@gmail.com>
2021-08-11 17:35:49 +0300

    TabLayout is created along with the main fragments

--- app/src/main/java/com/jaffer/mad/MainActivity.java
+++ app/src/main/java/com/jaffer/mad/MainActivity.java
@@ -2,7 +2,12 @@ package com.jaffer.mad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Inflate the toolbar
+        Toolbar toolbar = findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        //Create instance of the tab layout
+        TabLayout tabLayout= findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.mental_health));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.addiction_recovery));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.previous_activities));
+        //set the tab to fill the layout
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        //Use the pager adapter to manage screens
+        //create an instance of the view pager
+        final ViewPager viewPager = findViewById(R.id.view_pager);
+        //create an instance of the PagerAdapter
+        final PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        //set the adapter to the viewpager
+        viewPager.setAdapter(pagerAdapter);
+        //set listener for Clicks
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 }
