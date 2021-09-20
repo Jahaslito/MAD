c3fcda24ec739cbf41f6e6029588ca04dcba946c
amengemeda <amentamene@gmail.com>
2021-09-20 20:51:10 +0300

    TabLayout is created along with the main fragments

--- app/src/main/java/com/jaffer/mad/PreviousActivitiesFragment.java
+++ app/src/main/java/com/jaffer/mad/PreviousActivitiesFragment.java
@@ -1,5 +1,6 @@
 package com.jaffer.mad;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -61,4 +62,6 @@ public class PreviousActivitiesFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_previous_activities, container, false);
     }
+
+
 }
